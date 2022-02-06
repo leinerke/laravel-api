f4ef2f24ade76ac652235369c47b3a583359b068
Leinerke Suarez <leinerke2000@gmail.com>
2022-02-06 01:13:27 -0500

    Versión 2: colección.

--- app/Http/Resources/V2/PostCollection.php
+++ app/Http/Resources/V2/PostCollection.php
@@ -9,11 +9,20 @@ class PostCollection extends ResourceCollection
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'Platzi',
+                'authors' => [
+                    'Leinerke Suarez'
+                ],
+                'type' => 'articles'
+            ]
+        ];
     }
 }
